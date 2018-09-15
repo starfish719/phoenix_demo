ded81f474ce8916a4efbe3960c53e53add0cad9b
toda <starfish719@gmail.com>
2018-09-15 12:22:44 +0900

    create seeds

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -9,3 +9,30 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+
+alias PhoenixDemo.Articles.Category
+alias PhoenixDemo.Repo
+
+init_categories = [
+  [
+    {:name, "カテゴリ1"},
+    {:color_code, "00ffff"}
+  ],
+  [
+    {:name, "カテゴリ2"},
+    {:color_code, "ff00ff"}
+  ],
+  [
+    {:name, "カテゴリ3"},
+    {:color_code, "ffff00"}
+  ],
+]
+
+for init_category <- init_categories do
+  Repo.insert!(
+    %Category{
+      name: init_category[:name],
+      color_code: init_category[:color_code]
+    }
+  )
+end
