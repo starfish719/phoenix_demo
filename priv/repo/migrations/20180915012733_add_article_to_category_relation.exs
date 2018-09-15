695f6e702a7b1272a6459555f6babca3ba6abd92
toda <starfish719@gmail.com>
2018-09-15 11:53:06 +0900

    Revert "add aritcle to category relation"
    This reverts commit 13c2d77b89145087641f125820a6a63ac54c20f7.

--- priv/repo/migrations/20180915012733_add_article_to_category_relation.exs
+++
@@ -1,9 +0,0 @@
-defmodule PhoenixDemo.Repo.Migrations.AddArticleToCategoryRelation do
-  use Ecto.Migration
-
-  def change do
-    alter table(:articles) do
-      add :category_id, references(:categories)
-    end
-  end
-end
